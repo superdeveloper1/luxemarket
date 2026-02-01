3b6145173efcd756cb659b4f1e73661782b9979a
Isaias Geronimo <mprii@hotmail.com>
2026-02-01 16:50:19 -0500

    Fix missing RelatedItems import

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
+import RelatedItems from './RelatedItems.jsx';
 
 function ProductDetail({ product, onClose, currentUser, onOpenAuth, onCartUpdate }) {
   const [selectedColor, setSelectedColor] = React.useState(null);
